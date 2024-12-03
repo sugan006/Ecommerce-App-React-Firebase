417de16954768573c90e60f28f800a84c96e4338
senthilkumaransu <senthilkumaransu@dpinfotech.lk>
2024-12-03 15:28:54 +0530

    Updated

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -3,13 +3,13 @@ import { getFirestore, collection, addDoc } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB0ydHeQ4C1eKByyh-mdrUA0A0n21XfSCY",
-  authDomain: "sample-firebase-project-470ef.firebaseapp.com",
-  projectId: "sample-firebase-project-470ef",
-  storageBucket: "sample-firebase-project-470ef.firebasestorage.app",
-  messagingSenderId: "825491338306",
-  appId: "1:825491338306:web:82b959327d09c594a1e9db",
-  measurementId: "G-3CSLF6WGTS"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
